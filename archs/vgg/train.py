5100ccf4531952a6b71c5e83eb61361d9564b20e
manosh7n <manoshin.dm@gmail.com>
2023-01-25 23:32:44 +1000

    train script upd

--- archs/vgg/train.py
+++ archs/vgg/train.py
@@ -11,7 +11,7 @@ import pytorch_lightning as pl
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from mlflow import log_metric, log_param, log_artifacts
-from torchmetrics.functional import auroc, accuracy, recall, precision, f1_score
+from torchmetrics import AUROC, Accuracy, Recall, Precision, F1Score
 
 from vgg import VGG
 from data.dataset import train_set, valid_set
@@ -23,6 +23,26 @@ class VGGpl(pl.LightningModule):
         super().__init__()
         
         self.model = VGG(**kwargs)
+        
+        common_params = dict(
+            task='multiclass', 
+            num_classes=200
+        )
+        
+        self.metric = {
+            'val_acc': Accuracy(**common_params),
+            'val_auroc_macro': AUROC(average='macro', **common_params),
+            'val_f1_micro': F1Score(average='micro', **common_params),
+            'val_f1_macro': F1Score(average='macro', **common_params),
+
+            'train_acc': Accuracy(**common_params),
+            'train_auroc_macro': AUROC(average='macro', **common_params),
+            'train_f1_micro': F1Score(average='micro', **common_params),
+            'train_f1_macro': F1Score(average='macro', **common_params),
+        }
+        
+        for k, v in self.metric.items():
+            self.metric[k] = self.metric[k].to('cuda')
     
     def training_step(self, batch, batch_idx):
         images, targets = batch
@@ -31,12 +51,22 @@ class VGGpl(pl.LightningModule):
         self.predicts = output.softmax(1).argmax(1)
         loss = nn.functional.cross_entropy(output, targets)
         
+        self.metric['train_acc'].update(self.predicts, targets)
+        
         return loss
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=LR)
         return optimizer
     
+    
+    def validation_step(self, batch, batch_idx):
+        images, targets = batch
+        
+        output = self.model(images)
+        self.predicts = output.softmax(1).argmax(1)
+        loss = nn.functional.cross_entropy(output, targets)
+        
 
 model = VGGpl({'config': MODEL_CFG, 'num_classes': 200})
 
