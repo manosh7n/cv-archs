a51301fea983b6a251d38af2be1fd1237a08608d
manosh7n <manoshin.dm@gmail.com>
2023-01-30 23:00:50 +1000

    resnet upd

---
+++ archs/resnet/modules.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class Bottleneck(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        pass
+        
--- archs/resnet/resnet.py
+++ archs/resnet/resnet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 from typing import List, Dict, Union
+from .modules import *
 
 
 class ResNet(nn.Module):
@@ -12,4 +13,12 @@ class ResNet(nn.Module):
                  init_weights: bool = False,
                  ) -> None:
         super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(7, 7),
+                               stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(True)
+        self.maxpool = nn.MaxPool2d(kernel_size=(3, 3), stride=2, padding=1)
         
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        pass
