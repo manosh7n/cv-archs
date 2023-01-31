09021bccbfa0cc8775eb8ca36fc8de8a383cf93a
manosh7n <manoshin.dm@gmail.com>
2023-02-01 00:35:39 +1000

    resnet upd

--- archs/resnet/modules.py
+++ archs/resnet/modules.py
@@ -47,6 +47,42 @@ class Bottleneck(nn.Module):
         
         return out
         
+
+class BasicResidualBlock(nn.Module):
+    def __init__(self,
+                 in_channels: int, 
+                 stride: int, 
+                 downsample_block: Optional[nn.Module] = None) -> None:
+        super().__init__()
+
+        self.downsample = downsample_block
+        
+        self.conv3x3_1 = nn.Conv2d(in_channels, in_channels, (3, 3), stride=stride, bias=False)
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        
+        self.conv3x3_2 = nn.Conv2d(in_channels, in_channels, (3, 3), stride=1, bias=False)
+        self.bn2 = nn.BatchNorm3d(in_channels)
+        
+        self.relu = nn.ReLU()
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+        
+        out = self.conv3x3_1(out)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.conv3x3_2(out)
+        out = self.bn2(out)
+        
+        if self.downsample:
+            identity = self.downsample(identity)
+        
+        out += identity
+        out = self.relu(out)
+        
+        return out
+        
         
         
         
