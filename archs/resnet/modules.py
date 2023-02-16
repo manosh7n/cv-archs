38a6e04d797ab63eec09a54feac847351db59a0c
manosh7n <manoshin.dm@gmail.com>
2023-02-17 00:36:43 +1000

    resnet upd

--- archs/resnet/modules.py
+++ archs/resnet/modules.py
@@ -8,20 +8,25 @@ class Bottleneck(nn.Module):
                  in_channels: int,
                  stride: int,
                  shortcut: Optional[nn.Module] = None,
-                 increase_factor: int = 4
+                 increase_factor: int = 4,
+                 first_pos: bool = False
                  ) -> None:
         super().__init__()
 
         self.downsample = shortcut
+        out_channels = in_channels
+        
+        if first_pos:
+            in_channels *= 2
 
-        self.conv1x1_1 = nn.Conv2d(in_channels, in_channels, (1, 1), stride=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.conv1x1_1 = nn.Conv2d(in_channels, out_channels, (1, 1), stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
 
-        self.conv3x3 = nn.Conv2d(in_channels, in_channels, (3, 3), stride=stride, bias=False, padding=1)
-        self.bn2 = nn.BatchNorm2d(in_channels)
+        self.conv3x3 = nn.Conv2d(out_channels, out_channels, (3, 3), stride=stride, bias=False, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
 
-        self.conv1x1_2 = nn.Conv2d(in_channels, in_channels * increase_factor, (1, 1), stride=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(in_channels * increase_factor)
+        self.conv1x1_2 = nn.Conv2d(out_channels, out_channels * increase_factor, (1, 1), stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels * increase_factor)
 
         self.relu = nn.ReLU()
 
@@ -53,16 +58,19 @@ class BasicResidualBlock(nn.Module):
                  in_channels: int,
                  stride: int,
                  shortcut: Optional[nn.Module] = None,
-                 increase_factor: int = 1
+                 increase_factor: int = 1,
+                 *args, **kwargs
                  ) -> None:
         super().__init__()
 
         self.downsample = shortcut
 
-        self.conv3x3_1 = nn.Conv2d(in_channels, in_channels, (3, 3), stride=stride, bias=False, padding=1)
+        self.conv3x3_1 = nn.Conv2d(in_channels, in_channels, (3, 3), 
+                                   stride=stride, bias=False, padding=1)
         self.bn1 = nn.BatchNorm2d(in_channels)
 
-        self.conv3x3_2 = nn.Conv2d(in_channels, in_channels * increase_factor, (3, 3), stride=1, bias=False, padding=1)
+        self.conv3x3_2 = nn.Conv2d(in_channels, in_channels * increase_factor, (3, 3), 
+                                   stride=1, bias=False, padding=1)
         self.bn2 = nn.BatchNorm2d(in_channels * increase_factor)
 
         self.relu = nn.ReLU()
@@ -94,7 +102,8 @@ class ShortcutBlock(nn.Module):
                  ) -> None:
         super().__init__()
 
-        self.conv = nn.Conv2d(in_channels, in_channels * increase_factor, (3, 3), stride, padding=1, bias=False)
+        self.conv = nn.Conv2d(in_channels, in_channels * increase_factor, (3, 3), 
+                              stride, padding=1, bias=False)
         self.bn = nn.BatchNorm2d(in_channels * increase_factor)
 
     def forward(self, x):
--- archs/resnet/resnet.py
+++ archs/resnet/resnet.py
@@ -21,9 +21,15 @@ class ResNet(nn.Module):
                  ) -> None:
         super().__init__()
         
+        if config in ['resnet50', 'resnet101', 'resnet152']:
+            self.basic_block = Bottleneck
+            self.block_inrease_factor = 4
+        else:
+            self.basic_block = BasicResidualBlock
+            self.block_inrease_factor =  2
+        
         self.channels = [64, 128, 256, 512]
-        self.basic_block = Bottleneck if config in ['resnet50', 'resnet101', 'resnet152'] else BasicResidualBlock
-
+        
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(7, 7),
                                stride=2, padding=3, bias=False)
         self.bn = nn.BatchNorm2d(64)
@@ -39,7 +45,8 @@ class ResNet(nn.Module):
         
         # global pool ?
         self.avg_pool = nn.AdaptiveMaxPool2d((1, 1))
-        self.classifier = nn.Linear(?, num_classes)
+        self.classifier = nn.Linear(self.channels[-1] * self.block_inrease_factor, 
+                                    num_classes)
         
         if init_weights:
             self._init_weights()
@@ -50,10 +57,12 @@ class ResNet(nn.Module):
         x = self.relu(x)
         x = self.maxpool(x)
         
+        # ?? todo
         for stage_block in self.stage_blocks:
             x = stage_block(x)
         
         x = self.avg_pool(x)
+        x = torch.flatten(x, 1)
         x = self.classifier(x)
         
         return x
@@ -61,27 +70,29 @@ class ResNet(nn.Module):
     def _make_stage_block(self,
                     in_channels: int,
                     block_count: int,
-                    block_num: int) -> nn.Module:
+                    stage_num: int) -> nn.Module:
         
         block = nn.Sequential()
-        shortcut = ShortcutBlock(in_channels, stride=2, increase_factor=2)
-
-        for i in range(1, block_count):
+        shortcut = ShortcutBlock(in_channels, stride=2, 
+                                 increase_factor=self.block_inrease_factor)
+        
+        for i in range(block_count - 1):
             block.add_module(
-                f'basic_block_{block_num}_{i}',
+                f'basic_block_{stage_num}_{i}',
                 self.basic_block(in_channels=in_channels,
                                  stride=1,
                                  shortcut=None,
-                                 increase_factor=1)
+                                 increase_factor=1,
+                                 first_pos=(i == 0) & (stage_num != 0))
             )
-        
         block.add_module(
-            f'basic_block_{block_num}_{block_count}',
+            f'basic_block_{stage_num}_{block_count}',
             self.basic_block(in_channels=in_channels,
                              stride=2,
                              shortcut=shortcut,
-                             increase_factor=2)
+                             increase_factor=self.block_inrease_factor)
             )
+        print(self.block_inrease_factor, in_channels)
             
         return block
     
